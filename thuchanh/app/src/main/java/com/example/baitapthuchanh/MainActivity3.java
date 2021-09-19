54707227e46426700f679f6c58f1e1d716394d83
Phung Thanh Toan <toanpt322@gmail.com>
2021-09-19 11:17:30 +0700

    Motion Layout

--- thuchanh/app/src/main/java/com/example/baitapthuchanh/MainActivity3.java
+++ thuchanh/app/src/main/java/com/example/baitapthuchanh/MainActivity3.java
@@ -3,12 +3,25 @@ package com.example.baitapthuchanh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity3 extends AppCompatActivity {
+    ImageView img_taylor, img_music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        img_music = findViewById(R.id.img_music);
+        img_taylor = findViewById(R.id.img_Taylor);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anirotate);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.aniblink);
+
+        img_taylor.startAnimation(animation);
+        img_music.startAnimation(animation1);
     }
 }
